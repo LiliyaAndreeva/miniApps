1c1e88ca69be72e4e22a1f156d02c183596f4ce6
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-09-08 13:23:37 +0500

    Initial Commit

---
+++ miniApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniApps
+//
+//  Created by Лилия Андреева on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
